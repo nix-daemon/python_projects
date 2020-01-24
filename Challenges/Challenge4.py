108e8b18edd8f6f32a334a6459d2339cf049cade
tankjesus <60205749+tankjesus@users.noreply.github.com>
2020-01-24 11:01:10 -0500

    Update Challenge4.py

--- Challenges/Challenge4.py
+++ Challenges/Challenge4.py
@@ -5,3 +5,7 @@
 #If it is a multiple of 12, do not print triangle or square.  Instead, print "x dozen" where x is the numeral representing
 #how many dozen you're at("1 dozen" for 12, "2 dozen" for 24, etc.)
 #Lastly, print the sum of the numbers between 1 and the argument (inclusive).
+def loop(num):
+  for i in range(1, (num+1)):
+    print(i)
+loop(num)

bcd21f5e37eea6d46dfdd7ff17b91744692f6457
tankjesus <60205749+tankjesus@users.noreply.github.com>
2020-01-27 09:16:01 -0500

    Update Challenge5.py

--- Challenges/Challenge5.py
+++ Challenges/Challenge5.py
@@ -3,14 +3,15 @@
 #is 4x3x2x1 = 24.  The factorial function should print the factorial value.  It should not return a value.
 import sys
 def factor(n):
+    x=1
     if n < 0:
         sys.exit("I'm sorry Dave, I'm afraid I can't do that")
     if n == 1 or n == 0:
         print(1)
     else:
         for i in range(1, n+1):
-            n = n * i
-        print(n)
+            x = x * i
+        print(x)
 
 if __name__ == "__main__":
     factor(int(sys.argv[1]))

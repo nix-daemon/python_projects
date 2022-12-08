91cdc9e0f92bba36a817030d1951b92e72146a9d
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-08 08:45:24 -0500

    Create settings.py

---
+++ alien_invasion/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store alls ettings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the games settings."""
+
+        # Screen settings
+        self.screen_width = 1550
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)

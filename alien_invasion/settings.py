6c0977bd5fb2cdc5318a8084b0588dbd9172226e
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-09 07:53:43 -0500

    Update settings.py

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -4,7 +4,10 @@ class Settings:
     def __init__(self):
         """Initialize the games settings."""
 
+        # Ship settings
+        self.ship_speed = 1.5
+
         # Screen settings
-        self.screen_width = 1550
+        self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)

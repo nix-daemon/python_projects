77ca5daea156bac3f309a7016816ececd929137e
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-13 07:34:19 -0500

    Update alien_invasion.py

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -29,11 +29,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_screen()
-            self.bullets.update()
-            # Get rid of bullets that have disappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
+            self._update_bullets()
 
     def _check_events(self):
         """Respond to keypresses and mouse"""
@@ -68,6 +64,16 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets"""
+        #Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""

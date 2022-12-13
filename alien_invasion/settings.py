f8437601b818eac32a88a60dc10989b754d6d4f9
nix-daemon <60205749+nix-daemon@users.noreply.github.com>
2022-12-13 07:26:49 -0500

    Update settings.py

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -15,5 +15,5 @@ class Settings:
         # Bullet settings
         self.bullet_speed= 1.0
         self.bullet_width = 3
-        self.bullet_heigh = 15
+        self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
